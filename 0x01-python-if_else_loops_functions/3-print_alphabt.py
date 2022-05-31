a879e4fc62d091638d06cd0688ee2c017a082a99
ogumaterry <ogumaterry@gmail.com>
2022-05-31 22:45:54 +0000

    python code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for chr in "abcdefghijklmnopqrstuvwxyz":
+    if chr == "q" or chr == "e":
+        continue
+    print('{}'.format(chr), end="")
