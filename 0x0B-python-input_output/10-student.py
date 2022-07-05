f798351e8a36b9ddac9e065666f98ea1e1fdbb05
ogumaterry <ogumaterry@gmail.com>
2022-07-05 22:49:42 +0000

    my python codes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-"""contains a class"""
+""" Module that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
+"""
 
 
-class Student:
-    """student class"""
+def class_to_json(obj):
+    """ Function that retuns the dictionary description of an obj """
 
-    def __int__(self, first_name, last_name, age):
-        """initialization of student object"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """retrieves  a dictionary representation of
-           a student instance
-        """
-        try:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return self.__dict__
-        except TypeError:
-            return self.__dict__
-        my_dict = dict()
-        for key, value in self.__dict__.items():
-            if key in attrs:
-                my_dict[key] = value
-        return my_dict
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
