4a0f46e469a7b05798cad99f937e166987c20aaf
Terryodib <terry.odib@gmail.com>
2022-10-26 23:01:48 +0000

    java

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,14 @@
+"use strict";
+$(() => {
+  $("DIV#toogle_header").click(() => {
+    if ($("header").hasClass("green")) {
+      $("header").removeClass("green");
+      $("header").addClass("red");
+    } else if ($("header").hasClass("red")) {
+      $("header").removeClass("red");
+      $("header").addClass("green");
+    } else {
+      $("header").addClass("red");
+    }
+  });
+});
